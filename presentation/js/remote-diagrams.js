0967afa48f5b6122dc8c0db0114cb41844e222f4
Andrew Hobden <andrew@hoverbear.org>
2017-04-03 14:42:05 +0200

    Use remote diagrams

---
+++ presentation/js/remote-diagrams.js
@@ -0,0 +1,38 @@
+fetchAllDiagrams = function(){
+	Array.prototype.slice.call(document.querySelectorAll('pre[class="diagram"][data-source]')).forEach(function(diagramContainer){
+		console.log(diagramContainer.dataset.source);
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", diagramContainer.dataset.source, true);
+		xhr.overrideMimeType("text/plain; charset=x-user-defined");
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+                    processDiagram(diagramContainer, xhr.responseText);
+				}
+				else
+				{
+					console.error(xhr.status)
+					console.error("Error while trying to get remote diagram");
+				}
+			}
+		};
+		try {
+			xhr.send(null);
+		} catch (e) {
+			console.error("XHR failed for " + url + ", " + e);
+		}
+	});
+}
+
+processDiagram = function(container, content) {
+    var svg = Module.process_string("demo", content, "rust diagram")
+    var parent = container.parentNode;
+
+    var template = document.createElement('template');
+    template.innerHTML = svg;
+    var svgElement = template.content.firstChild;
+
+    parent.removeChild(container);
+    parent.appendChild(svgElement);
+    
+}
