841b22b7facb159efdffd928aac31adfa21eb2d4
Florian Gilcher <florian.gilcher@asquera.de>
2017-05-16 13:15:35 +0200

    Change sublifetimes example

--- presentation/chapters/shared/code/lifetimes/4.rs
+++ presentation/chapters/shared/code/lifetimes/4.rs
@@ -1,17 +1,36 @@
-struct Container<'a, T: 'a> {
-    inner: &'a T
+use std::str::Split;
+
+struct Tokenizer<'i> {
+    input: Split<'i, char>,
 }
 
-struct Worker<'a, 'b: 'a, T: 'b> {
-    workload: &'a Container<'b, T>
+impl<'i> Tokenizer<'i> {
+    fn next_token(&mut self) -> Option<&'i str> {
+        self.input.next()
+    }
 }
 
-fn create_worker<'a, 'b: 'a, T>(container: &'a Container<'b, T>) -> Worker<'a, 'b, T> {
-    Worker { workload: container }
+struct Parser<'t, 'i: 't> {
+    tokenizer: &'t mut Tokenizer<'i>,
+}
+
+impl<'t, 'i: 't> Parser<'t, 'i> {
+    fn next_item(&mut self) -> Option<&'i str> {
+        self.tokenizer.next_token()
+    }
 }
 
 fn main() {
-    let item = 42;
-    let c = Container { inner: &item };
-    let w = create_worker(&c);
+    let mut tok = Tokenizer { input: "( foo bar )".split(' ') };
+    let mut parser = Parser { tokenizer: &mut tok };
+
+    println!("{:?}", parser.next_item());
+    let content = parser.next_item();
+    let content2 = parser.next_item();
+    println!("{:?}", parser.next_item());
+    drop(parser);
+
+    println!("{:?}", content);
+    println!("{:?}", content2);
+
 }
