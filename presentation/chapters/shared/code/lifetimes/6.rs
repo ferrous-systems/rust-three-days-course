e769f26831621910c04cd1325078e8b0b1df71ad
Florian Gilcher <florian.gilcher@asquera.de>
2017-06-14 10:33:45 +0200

    Change lingo in lifetimes chapter

--- presentation/chapters/shared/code/lifetimes/6.rs
+++ presentation/chapters/shared/code/lifetimes/6.rs
@@ -1,5 +1,9 @@
-// str::split_at
+// slice::split_at
 
-fn split_at(&'a self, mid: usize) -> (&'a str, &'a str)
+fn split_at<T>(&[T], mid: usize) -> (&[T], &[T])
+
+}
+
+fn split_at<'a, T>(&'a [T], mid: usize) -> (&'a [T], &'a [T])
 
 }
