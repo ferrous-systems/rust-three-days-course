4a6694660116b2fbf5e9540aa27a3fa3041c60b3
Florian Gilcher <florian.gilcher@asquera.de>
2017-11-07 14:25:27 +0100

    Fix traits chapter code

--- presentation/chapters/shared/code/traits/4.rs
+++ presentation/chapters/shared/code/traits/4.rs
@@ -4,18 +4,18 @@ struct Point {
 }
 
 trait Distance {
-    fn distance(self, other: Self) -> i32;
+    fn distance(&self, other: &Self) -> f64;
 }
 
 impl Distance for Point {
     // Incorrect
-    fn distance(self, other: Point) -> i32 {
-        (self.x - other.x) + (self.y - other.y)
+    fn distance(&self, other: &Point) -> f64 {
+        (((other.x - self.x).pow(2) + (other.y - self.y).pow(2)) as f64).sqrt()
     }
 }
 
 fn main() {
     let p1 = Point { x: 1, y: 1 };
     let p2 = Point { x: 2, y: 2 };
-    println!("{}", p1.distance(p2));
+    println!("{}", p1.distance(&p2));
 }
