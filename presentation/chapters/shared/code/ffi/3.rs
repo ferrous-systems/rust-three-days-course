a323584d705d8b693ba948659689fbad840982cf
Florian Gilcher <florian.gilcher@asquera.de>
2017-12-01 09:24:36 +0100

    Use the modern way of opaque types

--- presentation/chapters/shared/code/ffi/3.rs
+++ presentation/chapters/shared/code/ffi/3.rs
@@ -1,3 +1,3 @@
-pub enum leveldb_t {}
-pub enum leveldb_comparator_t {}
-pub enum leveldb_options_t {}
+pub struct leveldb_t { _priv: [i32;0] }
+pub struct leveldb_comparator_t { _priv: [i32;0] }
+pub struct leveldb_options_t { _priv: [i32;0] }
