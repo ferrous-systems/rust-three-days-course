99ab9e77354ee27f849910cc8224e05bf506c97c
Katharina Fey <kookie@spacekookie.de>
2018-09-05 16:04:52 +0200

    Completely reworking the "outlook" chapter
    Most of the RFCs mentioned in the old chapter were already stable
    or still being blocked and uninteresting.
    
    The new chapter focuses on Rust 2018 and what the editions mean
    for the development of the language in the future (past 2018)
    with some examples of what has changed

--- presentation/chapters/shared/code/outlook/1.rs
+++ presentation/chapters/shared/code/outlook/1.rs
@@ -1,14 +1,5 @@
-pub trait Extend<A, T: IntoIterator<Item=A>> {
-    fn extend(&mut self, iterable: T);
-}
-
-// The generic implementation
-impl<A, T> Extend<A, T> for Vec<A> where T: IntoIterator<Item=A> {
-    // the `default` qualifier allows this method to be specialized
-    default fn extend(&mut self, iterable: T) {}
-}
-
-// A specialized implementation for slices
-impl<'a, A> Extend<A, &'a [A]> for Vec<A> {
-    fn extend(&mut self, iterable: &'a [A]) {}
-}
+|-src
+  |- main.rs
+  |- foo.rs
+  |- foo/
+     |- bar.rs
--- presentation/chapters/shared/code/outlook/3.rs
+++ presentation/chapters/shared/code/outlook/3.rs
@@ -1,10 +1,7 @@
-// [T; N] is a constructor, T → usize → 𝓤 
-// (parameterize over T and you get A → 𝓤).
-fn foo<const n: usize, const l: [u32; n]>() -> [u32; n] {
-    // ^ note how l depends on n.
-    l
-}
+use some_library::foo;
+use crate::foo::bar;
 
-// We know n from the length of the array.
-let l = foo::<_, [1, 2, 3, 4, 5, 6]>();
-//            ^   ^^^^^^^^^^^^^^^^
+fn main() {
+    foo(); // from `some_library`
+    bar(); // from `foo` module
+}
