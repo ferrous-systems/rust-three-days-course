3264659c5449b0ff40fab11e6f03e736f7e6b0cc
Katharina Fey <kookie@spacekookie.de>
2018-10-01 16:57:23 +0200

    Cleaning up teh crates chapter

--- presentation/chapters/shared/code/crates/1.rs
+++ presentation/chapters/shared/code/crates/1.rs
@@ -1,7 +1,11 @@
 fn main() {
-	use serde_json::{self, Value};
+    use serde_json::{self, Value};
 
     let data = r#" { "name": "John Doe", "age": 43, ... } "#;
     let v: Value = serde_json::from_str(data)?;
-    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
+    println!(
+        "Please call {} at the number {}", 
+        v["name"], 
+        v["phones"][0]
+    );
 }
